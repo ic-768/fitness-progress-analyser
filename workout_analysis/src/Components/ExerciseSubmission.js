22a7c9016b84a5b4626de64495d0d1b152ee6381
Ioannis Chrysostomakis <ic768@protonmail.com>
2021-02-20 19:36:10 +0200

    Exercise Submission verifies workout validity

--- workout_analysis/src/Components/ExerciseSubmission.js
+++ workout_analysis/src/Components/ExerciseSubmission.js
@@ -10,12 +10,21 @@ const ExerciseSubmission=({daysExercises})=>{
 	const history=useHistory()
 
 	const submitWorkout=async ()=>{
-		const userWorkouts = JSON.parse(window.localStorage.getItem("userWorkouts")) //local storage copy of workouts 
-		if (newWorkout){ //! MUST FILTER THROUGH TO MAKE SURE NO UNDEFINED EXERCISES
+
+		const cleanedWorkout = Object.keys(newWorkout) // remove undefined entries
+			.filter((exercise) =>  
+				newWorkout[exercise] != null) 
+			.reduce((workout, exercise) => ({ ...workout, [exercise]: newWorkout[exercise] }), {})
+
+		if (Object.entries(cleanedWorkout).length>0)
+		{//if contains a valid entry
 			const sentWorkout=await exerciseService.sendWorkout(newWorkout) //server response to new workout submission 
+			const userWorkouts = JSON.parse(window.localStorage.getItem("userWorkouts")) //local storage copy of workouts 
 			window.localStorage.setItem("userWorkouts",JSON.stringify(userWorkouts.concat(sentWorkout))) //update local Storage
-			history.push("/")
-		}}
+			history.push("/") 
+		} 
+		console.log("Couldn't find a single submitted exercise :/")
+	}
 
 
 	return ( 
